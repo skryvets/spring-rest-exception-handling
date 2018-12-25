81bcc4c665531d476641be094bf3ad76b589840e
skryvets <s.kryvets@gmail.com>
2018-12-25 08:36:15 -0600

    Initial commit

---
+++ src/main/java/com/example/demo/hello/GreetingController.java
@@ -0,0 +1,19 @@
+package com.example.demo.hello;
+
+import com.example.demo.models.Greeting;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name") String name) {
+        return new Greeting(String.format(template, name));
+    }
+}
---
+++ src/main/java/com/example/demo/models/Greeting.java
@@ -0,0 +1,14 @@
+package com.example.demo.models;
+
+public class Greeting {
+
+    private final String content;
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
