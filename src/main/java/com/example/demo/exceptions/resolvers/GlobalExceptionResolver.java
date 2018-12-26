8a70f139a320cd7cc4e4c59ee835b33fa6aa166a
skryvets <s.kryvets@gmail.com>
2018-12-26 08:49:23 -0600

    Add GlobalExceptionResolver

---
+++ src/main/java/com/example/demo/exceptions/resolvers/GlobalExceptionResolver.java
@@ -0,0 +1,16 @@
+package com.example.demo.exceptions.resolvers;
+
+import org.springframework.web.bind.annotation.*;
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+
+@RestControllerAdvice
+public class GlobalExceptionResolver {
+
+    @ExceptionHandler(Exception.class)
+    public HashMap<String, String> handleException(HttpServletRequest request, Exception e) {
+        HashMap<String, String> response = new HashMap<>();
+        response.put("message", e.getMessage());
+        return response;
+    }
+}
